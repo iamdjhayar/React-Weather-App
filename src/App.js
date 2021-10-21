de89f1d064e50a0570a23ae8d0a3508adcd645bd
dagooc.jayar@gmail.com <dagooc.jayar@gmail.com>
2021-10-21 13:02:31 +0800

    initialize dependecies(tailwind css)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const key = "19bb8f46b9b8c5df003baea3570c989b";
+  const [latitude,setLatitude] = useState(0)
+  const [longitude,setLongitude] = useState(0)
+  
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+      setLatitude(() => position.coords.latitude)
+      setLongitude(() => position.coords.longitude)
+      
+    });
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid='+key)
+      .then(res => {
+      return res.json();
+    }).then(function(res) {
+      console.log(res);
+    });
+
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button className="rounded-md bg-color-blue ">hello</button>
     </div>
-  );
+  )
 }
 
 export default App;
